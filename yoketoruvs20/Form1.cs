f5d7853110a4fec8374b4859151e2655c571ac26
A601-25 <tnk.dat.class@gmail.com>
2020-09-17 11:46:24 +0900

    ラベルの初期化

--- yoketoruvs20/Form1.cs
+++ yoketoruvs20/Form1.cs
@@ -24,7 +24,7 @@ namespace yoketoruvs20
         const int EnemyIndex = PlayerIndex + PlayerMax;
         const int ItemIndex = EnemyIndex + EnemyMax;
 
-        const string PlayerText = "(｀・ω・´)";
+        const string PlayerText = "(・ω・)";
         const string EnemyText = "🔶";
         const string ItemText = "★";
 
@@ -47,6 +47,25 @@ namespace yoketoruvs20
         public Form1()
         {
             InitializeComponent();
+
+            for(int i=0; i< ChrMAx; i++)
+            {
+                chrs[i] = new Label();
+                chrs[i].AutoSize = true;
+                if(i == PlayerIndex)
+                {
+                    chrs[i].Text = PlayerText;
+                }
+                else if(i < ItemIndex)
+                {
+                    chrs[i].Text = EnemyText;
+                }
+                else
+                {
+                    chrs[i].Text = ItemText;
+                }
+                Controls.Add(chrs[i]);
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
